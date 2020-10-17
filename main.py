7a8ed3ac412e6eb7f63d6fcb01874ef34cbea15d
405218718 <55682293+405218718@users.noreply.github.com>
2020-10-17 19:57:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Menu import zhujiemian_UI
 
 class dengluUI(QMainWindow):
     def __init__(self, parent=None):
-        # 加载方法三
+        # 加载方法三123
         super().__init__(parent)  # 调用父类构造函数，创建窗体
         self.ui = Ui_Form()  # 创建UI对象
         self.ui.setupUi(self)  # 构造UI界面

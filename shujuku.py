aca6e77f5e7c4999c669e55f008b239928c9a22d
fanwei <fanwei_zeng@163.com>
2020-11-18 21:35:36 +0800

    数据库功能

--- shujuku.py
+++ shujuku.py
@@ -60,7 +60,7 @@ def create_sql_db(self, conn, cursor, dbname):
 
 
 # 创建tablename表库
-def create_sql_tb(conn, cursor, tablename, dataframe, col_limit=''):
+def create_sql_tb(conn, cursor, tablename, dataframe:list, col_limit=''):
     """
     :param conn: 连接符
     :param cursor: 游标
@@ -69,17 +69,18 @@ def create_sql_tb(conn, cursor, tablename, dataframe, col_limit=''):
     :param col_limit: 对列的其他限制
     :return: 创建情况
     """
-    list_Type, list_Len = find_dfm_cols_most(dataframe)
-    print(list_Type, list_Len)
-    list_colname = list(dataframe.columns)  # 新表的列名为数据框的列名
-    list_col_desc = []  # 列描述,sql语言
-    for i in range(len(list_colname)):
-        print('正创建列:\t{} '.format(list_colname[i]))
-        # col_limit = input('Please enter the limits of the column:\t')
-        col_desc = list_colname[i] + ' ' + list_Type[i] + '(' + str(list_Len[i]) + ')' + col_limit
-        list_col_desc.append(col_desc)
-    sql = 'CREATE TABLE %s' % (tablename) + '(' + ','.join(list_col_desc) + ')'  # 用于执行的sql语句
-    sql = sql.replace('datetime(0)', 'datetime')
+    # list_Type, list_Len = find_dfm_cols_most(dataframe)
+    # print(list_Type, list_Len)
+    # list_colname = dataframe  # 新表的列名为数据框的列名
+    # list_col_desc = []  # 列描述,sql语言
+    # for i in range(len(list_colname)):
+    #     print('正创建列:\t{} '.format(list_colname[i]))
+    #     # col_limit = input('Please enter the limits of the column:\t')
+    #     col_desc = list_colname[i] + ' ' + list_Type[i] + '(' + str(list_Len[i]) + ')' + col_limit
+    #     list_col_desc.append(col_desc)
+    sql = 'CREATE TABLE %s' % tablename + '(`id` varchar(100) COLLATE utf8_estonian_ci NOT NULL COMMENT,' + \
+          'varchar(255) COLLATE utf8_estonian_ci DEFAULT NULL,'.join(dataframe) + ')'  # 用于执行的sql语句
+    # sql = sql.replace('datetime(0)', 'datetime')
     try:
         # print(sql)
         cursor.execute(sql)
@@ -89,8 +90,10 @@ def create_sql_tb(conn, cursor, tablename, dataframe, col_limit=''):
         if select == 'Y' or 'y':
             sql1 = 'drop table if exists {}'.format(tablename)  # 删除表
             cursor.execute(sql1)
-            sql = 'CREATE TABLE %s' % (tablename) + '(' + ','.join(list_col_desc) + ')'
-            sql = sql.replace('datetime(0)', 'datetime')
+            sql = 'CREATE TABLE %s' % tablename + '(`id` varchar(100) COLLATE utf8_estonian_ci NOT NULL COMMENT,' + \
+                  'varchar(255) COLLATE utf8_estonian_ci DEFAULT NULL,'.join(dataframe) + ')'  # 用于执行的sql语句
+            # sql = 'CREATE TABLE %s' % (tablename) + '(' + ','.join(list_col_desc) + ')'
+            # sql = sql.replace('datetime(0)', 'datetime')
             cursor.execute(sql)
             print('Rebuild table:\t{}'.format(tablename))
         elif select == 'N'or'n':
